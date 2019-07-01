2289771e936c73c97c3ddf23b0c1b0f2ea355c6c
Christopher Dang <dchrisu@yahoo.com>
2019-06-30 22:38:08 -0500

    repo update

---
+++ my-app/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Button variant="contained" className={classes.button}>
+                    Hello world
+                </Button>
+            </div>
+        );
+    }
+}
+export default Home;
---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//Pages
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App" style={{ minHeight: '100vh', paddingBottom: '200px', position: 'relative' }}>
+
+        </div>
+      </Router >
+    );
+  }
+}
+
+
+export default App;
